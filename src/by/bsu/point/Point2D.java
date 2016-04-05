1153a1343da9334fb967bc469ee67f13a62ff71f
azuzugonzales <slipknot_13@mail.ru>
2016-04-05 17:17:41 +0300

    new

---
+++ src/by/bsu/point/Point2D.java
@@ -0,0 +1,16 @@
+package by.bsu.point;
+public class Point2D extends Point1D {
+	private int y;
+	public Point2D(int x, int y){
+		super(x);
+		this.y = y;
+	}
+	@Override
+	public double length(){
+		return Math.hypot(super.length(), y);
+	}
+	@Override
+	public String toString(){
+		return super.toString() + " y = " + y;
+	}
+}
---
+++ src/by/bsu/point/Runner.java
@@ -0,0 +1,4 @@
+package by.bsu.point;
+public class Runner{
+	public static void main(S)
+}
