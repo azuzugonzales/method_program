6e4949a4f61fafccdd75df1e81c0359a202968e7
azuzugonzales <slipknot_13@mail.ru>
2016-04-06 17:49:20 +0300

    point2

---
+++ src/by/bsu/creator/BuildRunner.java
@@ -0,0 +1,9 @@
+package by.bsu.creator;
+public class BuildRunner{
+	public static void main(String[] args){
+		Point2DCreator br = new Point3DCreator();
+		Point2D p = br.createPoint();
+		System.out.println(br.createPoint().x);
+		System.out.println(br.createPoint().y);
+	}
+}
