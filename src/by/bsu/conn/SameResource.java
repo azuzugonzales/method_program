b080e3afc22ffe978aff596bed1a7cd436bc1a72
azuzugonzales <slipknot_13@mail.ru>
2016-04-22 15:02:20 +0300

    exception_1

---
+++ src/by/bsu/conn/SameResource.java
@@ -0,0 +1,2 @@
+package by.bsu.conn;
+public class SameResource{}
---
+++ src/by/bsu/exception/NestedTryCatchRunner.java
@@ -0,0 +1,15 @@
+package by.bsu.exception;
+public class NestedTryCatchRunner{
+	try{
+		int a = (int) (Math.random() * 2) -1;
+		System.out.println("a = " + a);
+		try{
+			int b = 1/a;
+			StringBuilder sb = new StringBuilder(a);
+		} catch(NegativeArraySizeException e) {
+			System.err.println("invalid buffer size: " + e);
+		}
+	} catch(ArithmeticException e){
+		System.err.println("division on 0: " + e);
+	}
+}
