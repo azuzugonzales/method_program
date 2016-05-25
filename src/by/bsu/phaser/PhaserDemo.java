4d3c610996d7ded2d42cc9a2780c4b29f344f71c
azuzugonzales <slipknot_13@mail.ru>
2016-05-25 23:27:34 +0300

    Thread_03

---
+++ src/by/bsu/phaser/PhaserDemo.java
@@ -0,0 +1,54 @@
+package by.bsu.phaser;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.Phaser;
+
+public class PhaserDemo {
+    public static void main(String[] args) {
+        Item[] goods = new Item[20];
+        for (int i = 0; i < goods.length; i++) {
+            goods[i] = new Item(i + 1);
+        }
+        List<Item> listGood = Arrays.asList(goods);
+        Storage storageA = Storage.createStorage(listGood.size(), listGood);
+        Storage storageB = Storage.createStorage(listGood.size());
+        Phaser phaser = new Phaser();
+        phaser.register();
+        int currentPhase;
+
+        Thread tr1 = new Thread(new Truck(phaser, "tr1", 5, storageA, storageB));
+        Thread tr2 = new Thread(new Truck(phaser, "tr2", 6, storageA, storageB));
+        Thread tr3 = new Thread(new Truck(phaser, "tr3", 7, storageA, storageB));
+        printGoodsToConsole("Product on sclad A", storageA);
+        printGoodsToConsole("Product on sclad B", storageB);
+        tr1.start();
+        tr2.start();
+        tr3.start();
+        // Synch load
+        currentPhase = phaser.getPhase();
+        phaser.arriveAndAwaitAdvance();
+        System.out.println("Load colonn complite. Phase " + currentPhase + " complite.");
+        // Synch drive
+        currentPhase = phaser.getPhase();
+        phaser.arriveAndAwaitAdvance();
+        System.out.println("Drive colonn complite. Phase " + currentPhase + " complite.");
+        // Synch unload
+        currentPhase = phaser.getPhase();
+        phaser.arriveAndAwaitAdvance();
+        System.out.println("Unload colonn complite. Phase " + currentPhase + " complite");
+
+        printGoodsToConsole("Product on sclad A", storageA);
+        printGoodsToConsole("Product on sclad B", storageB);
+    }
+
+    public static void printGoodsToConsole(String title, Storage storage) {
+        System.out.println(title);
+        Iterator<Item> goodIterator = storage.iterator();
+        while (goodIterator.hasNext()) {
+            System.out.print(goodIterator.next().getRegistrationNumber() + " ");
+        }
+        System.out.println();
+    }
+}
