b244e6dac41f569e97c767a624fc3f6252a74d8d
azuzugonzales <slipknot_13@mail.ru>
2016-05-25 17:38:04 +0300

    Thread 25 05_2

--- src/by/bsu/phaser/Truck.java
+++ src/by/bsu/phaser/Truck.java
@@ -5,14 +5,14 @@ import java.util.Random;
 import java.util.concurrent.Phaser;
 public class Truck implements Runnable{
 	private Phaser phaser;
-	private String name;
+	private String number;
 	private int capacity;
 	private Storage storageFrom;
 	private Storage storageTo;
 	private Queue<Item> bodyStorage;
 	public Truck(Phaser phaser, String name, int capacity, Storage stFrom, Storage stTo){
 		this.phaser = phaser;
-		this.name = name;
+		this.namber = name;
 		this.capacity = capacity;
 		this.bodyStorage = new ArrayDeque<Item>(capacity);
 		this.storageFrom = stFrom;
@@ -36,7 +36,33 @@ public class Truck implements Runnable{
 				return;
 			}
 			bodyStorage.add(g);
-			System.out.println("Truck " + number + " load product ¹")
+			System.out.println("Truck " + number + " load product ¹"
+								+ g.getRegistrationNumber());
+			try{
+				Thread.sleep(50);
+			} catch(InterruptedException e){
+				e.printStackTrace();
+			}					
 		}
+	}
+	private void unlockTruck(){
+		int size = bodyStorage.size();
+		for(int i = 0l i < size; i++){
+			Item g = bodyStorage.poll();
+			storageTo.setGood(g);
+			System.out.println("Truck " + number + " unloaded product ¹"
+								+ g.getRegistrationNumber());
+			try{
+				Thread.sleep(50);
+			} catch(InterruptedException e){
+				e.printStackTrace();
+			}					
+			
+	private void goTruck(){
+			
+			System.out.println("Truck " + number + " begin drive.");
+			Thread.sleep(new Random(100).nextInt(1000));
+			System.out.println("Truck " + number + " end drive.");
+		 c
 	}
 }
